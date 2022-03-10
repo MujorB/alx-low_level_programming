586c146dad988ed7b8f1c6d1ca865445e2f9a602
Bright Mujor <mujorb@gmail.com>
2022-03-10 15:16:14 -0800

    wo i am tired

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include<stdio.h> 
+#include <stdio.h>
 
 /**
  *main - Entry point
  *
  * Discription: prints - "Programming is like building a multilingual puzzle
  *
- * rwturns: Always 0 (Success)
+ * return: Always 0 (Success)
  */
-int main(void)
+int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
