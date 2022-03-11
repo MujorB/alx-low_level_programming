8dd9212afd2e5f085aea19a0cd8f5cd8230e9e4b
Bright Mujor <mujorb@gmail.com>
2022-03-11 07:36:39 -0800

    Task zero correction

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,20 +11,20 @@
 int main(void)
 {
 	int n;
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /* Gives the real signs of random num generated */
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* Gives the real signs of random num generated */
 	if(n < 0)
-        {
-                printf("is negative\n");
+	{
+		printf("%d is negative\n, n");
 	}
-          else if(n == 0)
-          {
-          	printf("is zero\n");
-          }
-          else
-          {
-		printf("is positive\n");
-          }				
+	else if(n == 0)
+	{
+		printf("%d is zero\n, n");
+	}
+	else
+	{
+		printf("%d is positive\n, n");
+	}
 	return (0);
 }
