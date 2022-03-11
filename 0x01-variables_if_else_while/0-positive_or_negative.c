7eea54bdb7c5cf5fbcbf0063d239dbbfed6f040a
Bright Mujor <mujorb@gmail.com>
2022-03-11 07:56:24 -0800

    Tasked 0 fixed now

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ int main(void)
 	/* Gives the real signs of random num generated */
 	if(n < 0)
 	{
-		printf("%d is negative\n, n");
+		printf("%d is negative\n", n);
 	}
 	else if(n == 0)
 	{
-		printf("%d is zero\n, n");
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is positive\n, n");
+		printf("%d is positive\n", n);
 	}
 	return (0);
 }
