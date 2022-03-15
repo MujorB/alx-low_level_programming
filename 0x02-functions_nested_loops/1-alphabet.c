56eebadd9647eb70f0fa957afc816a692d4cc690
Bright Mujor <mujorb@gmail.com>
2022-03-15 08:34:47 -0700

    Task One Completed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Description: Used for loop to make the iterations
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+
+	int i;
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	
+	return(0);
+}
