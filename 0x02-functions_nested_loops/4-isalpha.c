16e504d20ebfaee3be2c1f79302632d0c91e1d5a
Bright Mujor <mujorb@gmail.com>
2022-03-15 10:13:39 -0700

    Task 4 Done

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,6 +4,8 @@
  * _islower - checks for lowercase or uppercase
  *
  * @c: c is an ascii character
+ * 
+ * Description: checks for lower/uppercase letters
  *
  * Return: 1 if lowercase or uppercase
  */
