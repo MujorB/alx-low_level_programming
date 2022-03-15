1ef14c64e4bee3683df4c1a4be73a25c2e38f1bf
Bright Mujor <mujorb@gmail.com>
2022-03-15 09:07:59 -0700

    Task 2 completed

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ *
+ * Description: Prints a-z on a newline
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void);
+{
+
+	int i;
+	int j;
+
+	for (i = 0; i < 10;i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar(\n);
+	}
+}
