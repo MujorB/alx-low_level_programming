e2b2fae58d74f5c9ab684b9f77816ce085d62cdf
Bright Mujor <mujorb@gmail.com>
2022-03-21 16:04:20 -0700

    Task 3 done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _puts - prints string
+ *
+ * @str: string
+ *
+ * Return: void
+ */
+int main(0)		
+{
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		++a;
+	}
+
+}
