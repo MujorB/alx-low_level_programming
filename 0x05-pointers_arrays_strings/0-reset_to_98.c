6b9bf0ee2245bc2b458b1910eb182b6d04ffc55d
Bright Mujor <mujorb@gmail.com>
2022-03-21 06:46:16 -0700

    Task zero done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Description: A function that points to a value
+ *
+ *  Return: 0
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98
+
+	return(0);
+}
