f20bfe93ac84df1d2fe9a6281d8024c33be99bb1
Bright Mujor <mujorb@gmail.com>
2022-03-21 19:00:31 -0700

    done

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,17 +11,16 @@
  *
  * Return: no return.
  */
-
 void print_array(int *a, int n)
-
 {
-	int i = 0;
-	for (; i < n; i++)
 
+	int i = 0;
+
+	for (; i < n; i++)
 	{
 		printf("%d", *(a + i));
 		if (i != (n - 1))
-		printf(", ");
+			printf(", ");
 	}
 printf("\n");
 
