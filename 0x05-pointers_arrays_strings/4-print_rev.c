49a61752a9462a594d562b585ca964a28b244e7a
Bright Mujor <mujorb@gmail.com>
2022-03-21 18:06:03 -0700

    task done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse order
+ * @s: is a pointer
+ * Return: reverse string (Success)
+ */                                  
+void print_rev(char *s)
+{
+
+        int count = 0;
+
+	while (c
+        {
+                if (s[count] == '\0')
+		break;
+                count++;
+	}
+	for (count--; count >= 0; count--)
+	{
+		_putchar(s[count]);
+	}
+       	_putchar('\n');
+
+}
