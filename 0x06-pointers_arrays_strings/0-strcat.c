646844629b9b1bd3528bcd84fa16c4554811bc53
Bright Mujor <mujorb@gmail.com>
2022-03-24 01:55:20 -0700

    Task zero error 2 fixed

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,9 +3,11 @@
 #include "main.h"
 
 /**
- * main-Entry point
+ * _strcat - To print two diff strings together
  *
- * Descriptioin: To print two diff strings together
+ * @dest: the string to be added to
+ *
+ * @src: the string to be added
  *
  * Return: 0
  */
@@ -14,5 +16,5 @@ char _strcat(char *dest, char *src)
 
 	char *contanate = strcat(char dest, char src);
 
-	return(contanate);
+	return (contanate);
 }
