4d6cd1ad3e927d55f69d60a8e7e474100f45fcbe
Bright Mujor <mujorb@gmail.com>
2022-03-24 00:42:33 -0700

    Task zero done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * main-Entry point
+ *
+ * Descriptioin: To print two diff strings together
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
+	char *contanate = _strcat(char *dest, char *src);
+
+	reteurn(0);
+}
