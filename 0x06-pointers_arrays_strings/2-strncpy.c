56fa4b517ce157636522458987eff7c096868a74
Bright Mujor <mujorb@gmail.com>
2022-03-24 05:09:56 -0700

    task two error fixed the sixth time

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * _strncpy - copies a string
  *
  * @dest: destination.
@@ -9,7 +8,7 @@
  * @src: source.
  *
  * @n: amount of bytes from src.
- * 
+ *
  * Return: the pointer to dest.
  */
 char *_strncpy(char *dest, char *src, int n)
