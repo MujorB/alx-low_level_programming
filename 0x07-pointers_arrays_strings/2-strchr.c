92e9634bd059c8bf9010298234006850150d9b91
Bright Mujor <mujorb@gmail.com>
2022-03-28 19:20:43 -0700

    Task two fixed for the fouth time

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -20,10 +20,10 @@ char *_strchr(char *s, char c)
 		{
 			return (s + i);
 		}
-		if (*(s + i) == c)
-		{
-			return (s + i);
-		}
+	}
+	if (*(s + i) == c)
+	{
+		return (s + i);
 	}
 	return ('\0');
 
