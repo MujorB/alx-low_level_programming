0c66a93f1897458a81a696c41ca335d94a2cb1c6
Bright Mujor <mujorb@gmail.com>
2022-03-30 08:57:47 -0700

    Task zero completed

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Fuction to print a sting followed by a new line
+ *
+ * @s: holds the strings to be printed
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		++i;
+		_puts_recursion(s + i);
+	}
+
+	_putchar ("\n");
+}
