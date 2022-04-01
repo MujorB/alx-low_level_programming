b5ea2f25f69f2f0922fb8b7728b51cfe6f8a83e9
Bright Mujor <mujorb@gmail.com>
2022-04-01 15:36:49 -0700

    Task three error fixed once

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,13 +13,15 @@
 int main(int argc, char *argv[])
 {
 
+	int mul;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int mul = (atoi(argv[1]) * atoi(argv[2]));
+	mul = (atoi(argv[1]) * atoi(argv[2]));
 
 	printf("%d\n", mul);
 
