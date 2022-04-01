21a4ca787ad6232bd4b8071c9d731a83e1e1cfd3
Bright Mujor <mujorb@gmail.com>
2022-04-01 10:55:24 -0700

    Task zero Completed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: gives the size of the array elements
+ *
+ * @argv: gives the elements in the array
+ *
+ * Return: (0) on success.
+ */
+int main(int argc, char *argv[])
+{
+
+	puts(argv[1]);
+
+	return (0);
+
+}
