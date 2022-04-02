72c50bc36854334000cb6249625367a9b824d399
Bright Mujor <mujorb@gmail.com>
2022-04-01 17:36:00 -0700

    Task four done

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,35 +3,33 @@
 #include <ctype.h>
 
 /**
- * main - a function in c that executes other functions
+ * main - adds positive numbers.
  *
- * @argc: counts arguements
+ * @argc: number of command line arguments.
  *
- * @argv: is the arguement vector
+ * @argv: array that contains the program command line arguments.
  *
- * Return: return 0 (Success)
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
 
-	int i, result = 0;
-
-	if (argc == 1)
-		printf("0\n");
+	int i, j, add = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		int x = strtol(argv[i], NULL, 10);
-
-		if (isdigit(argv[i]) == 0)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		result = result + x;
+		add += atoi(argv[i]);
 	}
+	printf("%d\n", add);
 
-	printf("%d\n", result);
 	return (0);
 
 }
