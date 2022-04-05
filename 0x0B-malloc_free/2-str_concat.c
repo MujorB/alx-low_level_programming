cfeb3b8261e6e30e96893057cf9e6847a8569fe8
Bright Mujor <mujorb@gmail.com>
2022-04-05 13:18:45 -0700

    Task two error fixed the second time

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,15 +18,15 @@ char *str_concat(char *s1, char *s2)
 	int len1 = 0;
 	int len2 = 0;
 	char *ptr_s;
-	len_sum;
+	int len_sum;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (i < s1[len1])
+	while (s1[len1])
 		len1++;
-	while (j < s2[len2])
+	while (s2[len2])
 		len2++;
 	len_sum = len1 + len2;
 	ptr_s = (char *)malloc(sizeof(char) * (len_sum + 1));
