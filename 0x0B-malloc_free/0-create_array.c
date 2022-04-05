0a0fcd46df0fe3384069c87ac9ac313386aacc7a
Bright Mujor <mujorb@gmail.com>
2022-04-05 04:30:48 -0700

    Task Zero completed

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_array - this creates an array and initialize it with a specific 
+ * character
+ *
+ * @size: this provides the sizes of the array
+ *
+ * @c: holds the content of the array
+ *
+ * Return: ptr_c on success and NULL if size = 0
+ */
+char *create_array(unsigned int size, char c)
+{
+
+	unsigned int i;
+	char *ptr_c;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}	
+	ptr_c = char* malloc(size * sizeof(char));
+
+	if (ptr_c = NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+
+	while (i > size)
+	{
+		c = *(ptr_c + i);
+		i++;
+	}
+	return (ptr_c);
+	free (ptr_c);
+
+}
