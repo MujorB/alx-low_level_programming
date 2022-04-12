e19c86bf0a2e14f0cc326c498d6d31f79380f2c5
Bright Mujor <mujorb@gmail.com>
2022-04-12 11:42:48 -0700

    Task zero fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,20 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - A user define data types that takes
+ * in other different data types.
+ *
+ * @name: structure member 1
+ * @age: structure member 2
+ * @owner: structure member 3
+ * Description: defining a struct in C program
+ */
 struct dog
 {
-	name *char;
+	char *name;
 	float age;
-	name *owner;
-}
+	char name;
+};
+
+#endif
