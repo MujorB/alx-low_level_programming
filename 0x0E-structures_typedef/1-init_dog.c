f2776d85a459ba8c7ba728743a1ea9a41e0e7f85
Bright Mujor <mujorb@gmail.com>
2022-04-12 12:52:24 -0700

    Task one fixed

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		struct dog (*d).name = name;
+		struct dog (*d).age = age;
+		struct dog (*d).owner = owner;
+	}
+}
+
+#endif
