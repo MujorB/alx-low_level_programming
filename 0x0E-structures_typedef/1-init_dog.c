849672a2f94a460aecbbad3b2ae587584772e062
Bright Mujor <mujorb@gmail.com>
2022-04-12 17:28:00 -0700

    Task One error fixed

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,14 @@
 #include "dog.h"
 
+/**
+ * init_dog - void function that initializes a variable
+ * @d: pointer to the address of the initialized variable
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Return: return an initialized variable.
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
@@ -9,5 +18,3 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		struct dog (*d).owner = owner;
 	}
 }
-
-#endif
