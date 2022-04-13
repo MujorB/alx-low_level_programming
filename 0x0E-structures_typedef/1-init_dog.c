34894bec869c9d9e018462852b260b0de6403467
Bright Mujor <mujorb@gmail.com>
2022-04-12 17:38:56 -0700

    Fixed

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,8 +13,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-		struct dog (*d).name = name;
-		struct dog (*d).age = age;
-		struct dog (*d).owner = owner;
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
 	}
 }
