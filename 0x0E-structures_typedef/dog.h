c40d3bdd573fc926175b024c0fca2c22b33f7228
Bright Mujor <mujorb@gmail.com>
2022-04-13 23:44:30 -0700

    Updated the header file once

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,7 +5,7 @@
  * @name: first member
  * @age: second member
  * @owner: third member
- * Description: no long cap
+ * Description: User define data type.
  */
 struct dog
 {
@@ -14,4 +14,9 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
