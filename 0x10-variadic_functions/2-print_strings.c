a791eb6f1727f929e4fd8a00b6d34a2fdca1aaed
Bright Mujor <mujorb@gmail.com>
2022-04-14 22:08:57 -0700

    Task two fixed fourth time

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,13 +17,12 @@ void prints_strings(const char *separator, const unsigned int n, ...)
 	va_start(str_arg, n);
 	for (i = 0; i < n; i++)
 	{
+		str = va_arg(str_arg, char *);
+		if (str == NULL)
+			str = "nil";
+		printf("%s", str);
 		if (separator == NULL)
 			separator = "";
-		if (va_arg(str_arg, char*) == NULL)
-			str = "nil";
-		else
-			str = va_arg(str_arg, char *);
-		printf("%s", str);
 		if (i <  n - 1)
 			printf("%s", separator);
 	}
