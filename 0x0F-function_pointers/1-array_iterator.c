cc2f5bd4902394e670f6815de04d391e4111aed4
Bright Mujor <mujorb@gmail.com>
2022-04-16 01:06:23 -0700

    Task One error fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,10 +11,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	if (array[i] && size != NULL)
+	{
+		size_t i;
 
-	size_t i;
-
-	for (i = 0; i < size; i++)
-		action(array[i]);
-
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
 }
