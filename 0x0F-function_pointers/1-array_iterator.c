81cdcef9ba2e3ebe4c9e6bb44d4226a90d36ce44
Bright Mujor <mujorb@gmail.com>
2022-04-16 01:01:00 -0700

    Task One fix

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - functions that iterate an array to print to stdout
+ * its index value(note that this is also callback functions)
+ * @array: contains the value to be formatted
+ * @size: contains the attay size
+ * @action: a function pointer
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+
+}
