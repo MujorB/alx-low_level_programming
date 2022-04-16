fe01cc2eed559af7e0062a12c6b17111f7e866bf
Bright Mujor <mujorb@gmail.com>
2022-04-16 02:51:51 -0700

    Task two completes

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searching for an integer with certain conditions
+ * @array: contains the interger
+ * @size: contains the array size
+ * @cmp: pointer to a function that makes the comparison
+ *
+ * Return: returns cmp on success 
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+
+	int i;
+
+	if (array == 0 || size == 0 || cmp == 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		cmp(array[i]);
+			return (i);
+	}
+	return (-1);
+
+}
