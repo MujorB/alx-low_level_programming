95e66e8b90780fbd8fba315dad6db1ec28b6bb04
Mujor Bright <95404943+MujorB@users.noreply.github.com>
2022-04-16 17:19:17 +0100

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,26 +1,63 @@
 #include "3-calc.h"
-#include <stdlib.h>
 /**
- * get_op_func - function to select the function based on operation
- * @s: string symbol
+ * op_mod - divides a with b and returns the remainder
+ * @a: an int.
+ * @b: an int.
  *
- * Return: pointer to function that returns int
+ * Return: the results of the operation
  */
-int (*get_op_func(char *s))(int, int)
+int op_mod(int a, int b)
 {
-	op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
-	};
-	int i;
-	
-	i = 0;
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
-		i++;
+	return (a % b);
+}
 
-	return (ops[i].f);
+/**
+ * op_div - returns the division of a with b
+ * @a: an int.
+ * @b: an int.
+ *
+ * @a: an int.
+  5  * @b: an int.
+  6  *
+  7  * Return: the results of the operation * Return: the results of the operation
+ */
+int op_div(int a, int b)
+{
+	return(a / b);
+}
+
+/**
+ * op_mul - returns the multiplication of a and b
+ * @a: an int.
+ * @b: an int.
+ *
+ * Return: the results of the operation
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_sub - returns the diff of a and b
+ * @a: an int.
+ * @b: an int.
+ *
+ * Return: the results of the operation
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_add - returns the addition of a and b
+ * @a: an int.
+ * @b: an int.
+ *
+ * Return: the results of the operation
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
 }
