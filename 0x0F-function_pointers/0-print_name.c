fb8cbfa02a37ffaede910d3e88f6526670b35d0e
Bright Mujor <mujorb@gmail.com>
2022-04-16 00:40:41 -0700

    Task One error fix 1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,6 +3,8 @@
 /**
  * print_name - is a function that prints a name
  * @name: holds the string being formatted
+ * @f: Pointer to function with an agument of type char returning void
+ *
  * Returns: void
  */
 void print_name(char *name, void (*f)(char *))
