c1eea764d77f6b47ad762ff325e968e817776774
Bright Mujor <mujorb@gmail.com>
2022-04-16 08:57:08 -0700

    Functions code

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+/**
+ * op_mod - divides a with b and returns the remainder
+ * @a: an int.
+ * @b: an int.
+ *
+ * Return: the results of the operation
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * op_div - returns the division of a with b
+ * @a: an int.
+ * @b: an int.
+ *
+ * @a: an int.
+  5  * @b: an int.
+  6  *
+  7  * Return: the results of the operation * Return: the results of the operation
+ */
+int op_div(int a, int b)
+{
+	return(a / b);
+}
+
+/**
+ * op_mul - returns the multiplication of a and b
+ * @a: an int.
+ * @b: an int.
+ *
+ * Return: the results of the operation
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_sub - returns the diff of a and b
+ * @a: an int.
+ * @b: an int.
+ *
+ * Return: the results of the operation
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_add - returns the addition of a and b
+ * @a: an int.
+ * @b: an int.
+ *
+ * Return: the results of the operation
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
