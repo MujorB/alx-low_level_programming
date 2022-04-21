8e4fc6cb48e552bbe56426d275eef8d643b3d4b1
Mujor Bright <mujorb@gmail.com>
2022-04-21 12:50:35 -0700

    Add header file

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+void free_list(list_t *head);
+list_t *add_node_end(list_t **head, const char *str);
+list_t *add_node(list_t **head, const char *str);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+
+#endif
