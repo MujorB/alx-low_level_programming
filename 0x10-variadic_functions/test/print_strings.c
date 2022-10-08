73527ea019e57750227ef0c0df27040783e5a3b1
Mujor Bright <mujorb@gmail.com>
2022-10-08 02:15:21 -0700

    Add test files

---
+++ 0x10-variadic_functions/test/print_strings.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+
+/**
+ * prints_strings - prints the strings given as arguments
+ * @separator: separate the strings
+ * @n: the number of argument strings to displayed
+ *
+ * Return: void.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+
+	va_list str_arg;
+	unsigned int i;
+	char *str;
+
+	va_start(str_arg, n);
+	if (n == 0)
+	{
+		if (!separator)
+
+		for (i = 0; i < n; i++)
+		{
+			str = va_arg(str_arg, char *);
+			if (!str)
+				printf("(nil)");
+			else
+				printf("%s", str);
+			if (i <  n - 1)
+				printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(str_arg);
+
+}
